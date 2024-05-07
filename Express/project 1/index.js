f1d7ce2e833d37c61e8630266c4d43cbf0d117d8
Sagar Kumar <sagarjar213@gmail.com>
2024-05-08 01:41:00 +0530

    added

--- Express/project 1/index.js
+++ Express/project 1/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const fs=require('fs');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -8,7 +9,35 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get('/',(req,res)=>{
-    res.render("index");
+    fs.readdir(`./files`,function(err,files){
+        res.render("index",{files:files});
+    })
+    
 })
+
+app.post('/create',function(req,res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+        res.redirect("/");
+    });
+})
+
+app.post('/edit',function(req,res){
+   fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+    res.redirect("/");
+   });
+})
+
+app.get('/file/:filename',(req,res)=>{
+   fs.readFile(`files/${req.params.filename}`,"utf-8",function(error,filedata){
+    res.render('show',{filename:req.params.filename,filedata:filedata});
+   });  
+})
+
+app.get('/edit/:filename',(req,res)=>{
+    res.render('edit',{filename:req.params.filename});
+})
+
+
+
 
 app.listen(3000);
