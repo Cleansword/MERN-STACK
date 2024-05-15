796bbb111640b69a64cfe20d599d12c53a2d4ee0
Sagar Kumar <sagarjar213@gmail.com>
2024-05-16 02:07:25 +0530

    added

--- Express/Project 2/app.js
+++ Express/Project 2/app.js
@@ -1,8 +1,23 @@
 const express=require('express');
-const app=express()
+const app=express();
+const path=require('path');
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get("/",(req,res)=>{
-    res.send("hello");
+    res.render("index");
 })
+
+app.get("/read",(req,res)=>{
+    res.render("read");
+})
+
+app.post("/create",(req,res)=>{
+    res.render("read");
+})
+
 app.listen(3000);
