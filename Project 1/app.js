ca9cb6d852d029cdc8b32a1c843be153f8787360
Sagar Kumar <sagarjar213@gmail.com>
2024-05-23 04:19:39 +0530

    added

--- Project 1/app.js
+++ Project 1/app.js
@@ -1,10 +1,15 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const ownersRouter=require("./routes/ownersRouter");
+const usersRouter=require("./routes/usersRouter");
+const productsRouter=require("./routes/productsRouter");
+
+
+
 const cookieParser=require("cookie-parser");
 
 const db=require("./config/mongoose-connection");
-
 app.set("view engine","ejs");
 
 app.use(express.static(path.join(__dirname,"public")));
@@ -13,9 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
-app.get("/",(res,req)=>{
-    req.send("hello");
-});
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
 
 
 app.listen(3000);
