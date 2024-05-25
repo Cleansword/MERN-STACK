ccae7cf46891b4cdf9445b877c531470d3635f09
Sagar Kumar <sagarjar213@gmail.com>
2024-05-25 23:27:54 +0530

    added

--- Project 1/routes/ownersRouter.js
+++ Project 1/routes/ownersRouter.js
@@ -1,8 +1,17 @@
 const express=require("express");
 const router=express.Router();
+// const ownerModel=require("../models/owner-model");
 
 router.get("/",(req,res)=>{
     res.send("hello");
 });
+
+if(process.env.NODE_ENV==="development");
+
+router.post("/create",function(req,res){
+    res.send("hello");
+})
+
+
 
 module.exports=router;
