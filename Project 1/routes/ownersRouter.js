9e81cc905381dfaeb4a74a141e82149bf38109e7
Sagar Kumar <sagarjar213@gmail.com>
2024-05-27 00:52:12 +0530

    added

--- Project 1/routes/ownersRouter.js
+++ Project 1/routes/ownersRouter.js
@@ -2,24 +2,30 @@ const express=require("express");
 const router=express.Router();
 const ownerModel=require("../models/owner-model");
 
+
+
+// if(process.env.NODE_ENV=="developemnt"){
+    router.post("/create",async function(req,res){
+        let owners=await ownerModel.find();
+        if(owners.length>0){
+            return res
+                .status(504)
+                .send("you dont have permission to create a new owner");
+        }
+        let {fullname,email,password}=req.body;
+        let createdOwner=await ownerModel.create({
+            fullname,
+            email,
+            password
+        });
+        res.status(201).send(createdOwner);
+       
+    
+    });
+// }
+
 router.get("/",(req,res)=>{
     res.send("hello");
-});
-
-router.post("/create",async function(req,res){
-    let owners=await ownerModel.find();
-    if(owners.length>0){
-        return res
-            .send(503)
-            .send("you dont have permission to craete a new owner");
-    }
-    let {fullname,email,password}=req.body;
-    let createdOwner=await ownerModel.create({
-        fullname,
-        email,
-        password
-    });
-    res.status(201).send(createdOwner);
 });
 
 
